19c39952d68fc7205f43d7e86acbd3737fed1a59
Omm Prakash Sahoo <antoniocolapso7@gmail.com>
2024-03-20 21:10:00 +0530

    Subscribe to yt op does cp on youtube

--- CodeChef Live Solutions/CodeChef Starters 126 Solutions/Freedom.cpp
+++ CodeChef Live Solutions/CodeChef Starters 126 Solutions/Freedom.cpp
@@ -1,43 +1,63 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <bits/stdc++.h>
+ using namespace std;
+ // STAY CALM
 
-void yt_op_does_cp() {
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
-    
-    int count = 0;
-    vector<int> v;
-    for (int i = 0; i < n; ++i) {
-        if (arr[i] == i + 1) {
-            count++;
-            if (i == n - 1 && count > 0) {
-                v.push_back(count);
-            }
-        } else {
-            if (count > 0) {
-                v.push_back(count);
-            }
-            count = 0;
-        }
-    }
-    
-    int ans = 0;
-    for (int i = 0; i < v.size(); ++i) {
-        ans += (v[i] / 2) + (v[i] % 2);
-    }
-    cout << ans << endl;
-}
+ #define int long long int
+ #define vi vector<int>
+ #define read(a) for(auto &i: a) cin>>i
+ // #define mod 998244353
+ #define mod 1000000007
+ // vector<vector<int>> m(200005);
+ vector<vector<int>> m2(200005);
+ vector<int> siz(200005);
+ vector<int> dist(200005);
 
-int main() {
-    int iterations;
-    cin >> iterations;
-    for (int i = 0; i < iterations; ++i) {
-        yt_op_does_cp();
+
+ signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t=1;
+  cin>>t;
+  // sieve();
+  while(t--){
+   int n;
+   cin>>n;
+   vi a(n,0);
+   read(a);
+   int ans=0;
+   // ai * aj = ai + 3aj 
+   // y= x/x-3 
+   map<int,int> m;
+   for(auto it:a){
+    m[it]++;
+   }
+   // ahead of it 
+   for(int i=0;i<n;i++){
+    if(a[i]<=3){m[a[i]]--;continue;}
+    int num=a[i];
+    int den=a[i]-3;
+    if(num%den!=0){m[a[i]]--;continue;}
+    int val=num/den;
+    if(val==a[i]){
+     if(m[val]-1>0){
+      ans=ans+m[val]-1;
+     }
+     // ans=ans+max(0LL,m[val]-1);
     }
-    return 0;
-}
+    else{
+     if(m[val]>0){
+      ans=ans+m[val];
+     }
+     // ans=ans+max(0LL,m[val]);
+    }
+
+    m[a[i]]--;
+    // cout<<i<<" "<<ans<<endl;
+   }
+   // edge case if same raha to 
+   cout<<ans<<endl;
+  
+   
+  }
+  return 0;
+ }
